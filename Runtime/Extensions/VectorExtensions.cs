8b94d1a55c8e77068936440b835ab71b5d8ad41b
Adrian K <goshki@gmail.com>
2021-10-12 12:48:31 +0200

    Version update to `1.2.0`.

---
+++ Runtime/Extensions/VectorExtensions.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Vigeo {
+
+    public static class VectorExtensions {
+
+        public static Vector3 To(this Vector3 @this, Vector3 target) =>
+            target - @this;
+        
+        public static Vector3 DirectionTo(this Vector3 @this, Vector3 target) =>
+            @this.To(target).normalized;
+        
+        public static Vector3 With(this Vector3 @this, float? x = null, float? y = null, float? z = null) =>
+            new Vector3(x ?? @this.x, y ?? @this.y, z ?? @this.z);
+
+        public static Vector3 Set(this Vector3 @this, float? x = null, float? y = null, float? z = null) => @this.Apply(vector =>
+            vector.Set(x ?? vector.x, y ?? vector.y, z ?? vector.z)
+        );
+        
+        public static Vector2 XY(this Vector3 @this) =>
+            new Vector2(@this.x, @this.y);
+
+        public static Vector2 XZ(this Vector3 @this) =>
+            new Vector2(@this.x, @this.z);
+    }
+}
